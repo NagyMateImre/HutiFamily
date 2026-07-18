6b499872547932d7ef38bfe1319e3e4c0d212bbd
glevente <114288278+GLevente44@users.noreply.github.com>
2026-07-18 22:13:49 +0200

    Add files via upload

---
+++ js/fooldal.js
@@ -0,0 +1,30 @@
+const body = document.querySelector("body"),
+      sidebar = document.querySelector(".sidebar"),
+      toggle = document.querySelector(".toggle"),
+      modeSwitch = document.querySelector(".toggle-switch"),
+      modeText = document.querySelector(".mode-text");
+
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+
+
+modeSwitch.addEventListener("click", () => {
+
+    body.classList.toggle("dark");
+
+    if(body.classList.contains("dark")) {
+
+        modeText.innerHTML = "Nappali mód";
+
+        body.style.backgroundImage = "url('../img/nightBg.jpg')";
+
+    } else {
+
+        modeText.innerHTML = "Éjszakai mód";
+
+        body.style.backgroundImage = "url('../img/dayBg.png')";
+    }
+
+});
